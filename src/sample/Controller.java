361c723574a94b303ba3022860b7e765f51e8025
mpoapostolis <mpoapostolis@gmail.com>
2018-03-04 02:44:09 +0200

    screen sizes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,33 +1,97 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Rectangle;
+import javafx.stage.FileChooser;
 import javafx.stage.Screen;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Controller {
+
+    private List<String> myList = new ArrayList<>();
+
     @FXML
     GridPane simulation;
 
-    @FXML
-    public void initialize() {
-        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+    private String getColor(int x){
+        if (x == 0){
+            return "#0000FF";
+        } else if (x < 200){
+            return "#3CB371";
+        } else if (x < 400){
+            return "#2E8B57";
+        } else if (x < 700){
+            return "#228B22";
+        }  else if (x < 1500){
+            return "#DEB887";
+        }  else if (x < 3500){
+            return "#CD853F";
+        }  else {
+            return "#915014";
+        }
+    }
 
-        double Width = primaryScreenBounds.getWidth() * 0.8 / 60;
-        double Height = primaryScreenBounds.getHeight() / 30;
+    @FXML
+    public void draw() {
+        simulation.getChildren().clear();
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        double Width = (primaryScreenBounds.getWidth()* 0.8) / 1.2  / 60;
+        double Height = primaryScreenBounds.getHeight() / 1.2/ 30;
+        String color = "#FFFFFF";
+        int altitude = 0;
         for (int i=0; i<30; i++){
             for (int j =0; j<60; j++ ) {
+                if(myList.size() > 0){
+                    altitude = Integer.parseInt(myList.get((i*30)+j));
+                    color = getColor(altitude);
+                    System.out.println(color);
+                }
                 StackPane sp = new StackPane();
                 sp.setPrefSize(Width, Height);
-                Label l = new Label(Integer.toString(i));
-                Rectangle border = new Rectangle(Width, Height);
-                border.setFill(null);
-                sp.getChildren().addAll(border,l);
+                sp.setStyle(String.format("-fx-background-color: %s", color));
+                Label l = new Label(Integer.toString(altitude));
+                sp.getChildren().addAll(l);
                 simulation.add(sp,j,i);
             }
         }
     }
+
+    @FXML
+    public void initialize() {
+        draw();
+    }
+
+        @FXML
+    public void test (ActionEvent event) throws IOException {
+
+        FileChooser fileChooser = new FileChooser();
+
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        //Show save file dialog
+        File file = fileChooser.showOpenDialog(null);
+        if(file != null){
+          String fullPath = file.getCanonicalPath();
+          Path path = Paths.get(fullPath);
+          Stream <String> lines = Files.lines(path);
+          lines.forEach(line -> Arrays.asList(line.split(",")).forEach(myList::add));
+          draw();
+        }
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,12 +10,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+    public double WIDTH = primaryScreenBounds.getWidth() / 1.2;
+    public double HEIGHT = primaryScreenBounds.getHeight() / 1.2;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, primaryScreenBounds.getWidth(), primaryScreenBounds.getHeight()));
+        primaryStage.setTitle("Simulator");
+        primaryStage.setScene(new Scene(root,WIDTH, HEIGHT));
         primaryStage.show();
     }
 
